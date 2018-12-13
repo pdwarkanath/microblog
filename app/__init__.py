4c35d7d15d5186e40d76ff14c5eb633e9a5baa58
pdwarkanath <dwarkanath.prabhu@gmail.com>
2018-12-12 23:14:53 -0600

    forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__)  # in from app import app, this is the second app. First is the folder in which this file is stored
 app.config.from_object(Config)
 
 from app import routes
