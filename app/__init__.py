1eaab9eb4fa05f4b0e306e0c4959a892f46a09eb
pdwarkanath <dwarkanath.prabhu@gmail.com>
2018-12-13 20:39:48 -0600

    add db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)  # in from app import app, this is the second app. First is the folder in which this file is stored
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
