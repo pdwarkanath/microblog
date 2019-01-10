09661bc750066706ec80450ffa9aabda9ef0572f
pdwarkanath <dwarkanath.prabhu@gmail.com>
2019-01-10 06:40:18 -0600

    removed emailing errors

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ login.login_view = 'login'
 from app import routes, models, errors
 
 if not app.debug:
+	"""
 	if app.config['MAIL_SERVER']:
 		auth = None
 	if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -31,7 +32,7 @@ if not app.debug:
 		credentials=auth, secure=secure)
 	mail_handler.setLevel(logging.ERROR)
 	app.logger.addHandler(mail_handler)
-
+	"""
 	if not os.path.exists('logs'):
 		os.mkdir('logs')
 	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
