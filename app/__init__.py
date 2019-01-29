3e9efb322987345ba84fa7ea971b37f488eac7ac
pdwarkanath <dwarkanath.prabhu@gmail.com>
2019-01-29 16:38:53 -0600

    cli for translations

--- app/__init__.py
+++ app/__init__.py
@@ -25,8 +25,8 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-	return request.accept_languages.best_match(app.config['LANGUAGES'])
-	#return 'es'
+	#return request.accept_languages.best_match(app.config['LANGUAGES'])
+	return 'es'
 
 from app import routes, models, errors
 """
